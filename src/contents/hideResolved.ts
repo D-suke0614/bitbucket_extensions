bf0c12431366349f5e5aeba69c8960bae88152d0
Daisuke <daisuke.kida@nijibox.co.jp>
2024-10-10 12:41:44 +0900

    format

--- src/contents/hideResolved.ts
+++ src/contents/hideResolved.ts
@@ -1,26 +1,26 @@
-import type { PlasmoCSConfig } from "plasmo"
+import type { PlasmoCSConfig } from 'plasmo'
 
-import { sleep } from "~src/utils/sleep"
-import { getBooleanFromStorage, setStorage } from "~src/utils/storage"
+import { sleep } from '~src/utils/sleep'
+import { getBooleanFromStorage, setStorage } from '~src/utils/storage'
 
 export const config: PlasmoCSConfig = {
   matches: [
-    "https://stash.sprocket3.systems/projects/*/pull-requests/*/overview"
+    'https://stash.sprocket3.systems/projects/*/pull-requests/*/overview'
   ]
 }
 
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-  if (req.action === "CLICK_BUTTON") {
+  if (req.action === 'CLICK_BUTTON') {
     const isHideResolved = req.isHide
     // main(isHideResolved)
   }
 })
 
 const createButton = (isHideResolved: boolean) => {
-  const button = document.createElement("button")
+  const button = document.createElement('button')
   const buttonProperties = {
-    textContent: isHideResolved ? "▼ show resolved" : "▼ hide resolved",
-    type: "button"
+    textContent: isHideResolved ? '▼ show resolved' : '▼ hide resolved',
+    type: 'button'
   }
   const buttonStyles = `
     margin-left: 5px;
@@ -31,36 +31,36 @@ const createButton = (isHideResolved: boolean) => {
   `
   Object.assign(button, buttonProperties)
   button.style.cssText = buttonStyles
-  button.classList.add("displayed-control-button")
+  button.classList.add('displayed-control-button')
 
-  button.addEventListener("mouseenter", () => {
-    button.style.textDecoration = "underline"
+  button.addEventListener('mouseenter', () => {
+    button.style.textDecoration = 'underline'
   })
 
-  button.addEventListener("mouseleave", () => {
-    button.style.textDecoration = "none"
+  button.addEventListener('mouseleave', () => {
+    button.style.textDecoration = 'none'
   })
 
   return button
 }
 
 const init = async () => {
-  console.log("wait in 500ms")
+  console.log('wait in 500ms')
   await sleep(500)
-  console.log("target element list", document.querySelectorAll(".file-comment"))
-  const isHideResolved = await getBooleanFromStorage("isHideResolved")
+  console.log('target element list', document.querySelectorAll('.file-comment'))
+  const isHideResolved = await getBooleanFromStorage('isHideResolved')
 
-  const commentedElements = document.querySelectorAll(".file-comment")
+  const commentedElements = document.querySelectorAll('.file-comment')
   for (let i = 0; i < commentedElements.length; i++) {
     // すでに表示切替用ボタンを追加済だったらreturn
-    if (commentedElements[i].querySelector(".displayed-control-button")) return
+    if (commentedElements[i].querySelector('.displayed-control-button')) return
 
     // resolvedかの判定に使う要素
-    const commentHeader = commentedElements[i].querySelector(".comment-header")
+    const commentHeader = commentedElements[i].querySelector('.comment-header')
     const resolvedBudge = commentHeader.querySelector(
       "div[role='presentation']"
     )
-    console.log("resolvedBudge", resolvedBudge)
+    console.log('resolvedBudge', resolvedBudge)
 
     if (resolvedBudge) {
       // 表示切替用ボタン
@@ -68,20 +68,20 @@ const init = async () => {
 
       // 省略する要素の取得、初期表示、イベント設定
       const fileContent =
-        commentedElements[i].querySelector<HTMLElement>(".file-content")
-      fileContent.style.display = isHideResolved ? "none" : "block"
-      button.addEventListener("click", () => {
+        commentedElements[i].querySelector<HTMLElement>('.file-content')
+      fileContent.style.display = isHideResolved ? 'none' : 'block'
+      button.addEventListener('click', () => {
         const defaultTargetDisplay = fileContent.style.display
         button.textContent =
-          defaultTargetDisplay === "none"
-            ? "▲ hide resolved"
-            : "▼ show resolved"
+          defaultTargetDisplay === 'none'
+            ? '▲ hide resolved'
+            : '▼ show resolved'
         fileContent.style.display =
-          defaultTargetDisplay === "none" ? "block" : "none"
+          defaultTargetDisplay === 'none' ? 'block' : 'none'
       })
 
       const fileHeader =
-        commentedElements[i].querySelector<HTMLElement>(".file-header")
+        commentedElements[i].querySelector<HTMLElement>('.file-header')
       fileHeader.appendChild(button)
     }
   }
