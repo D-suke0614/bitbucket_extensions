2f3014adadf30898a251eec4bb3d736e5316cc8f
Daisuke <daisuke.kida@nijibox.co.jp>
2025-01-06 19:02:12 +0900

    format

--- src/contents/contents.ts
+++ src/contents/contents.ts
@@ -2,11 +2,11 @@ import { hideResolved } from "./hideResolved"
 import { protectMergeButton } from "./protectMergeButton"
 
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-  switch(req.action) {
-    case 'HIDE_RESOLVED':
+  switch (req.action) {
+    case "HIDE_RESOLVED":
       hideResolved()
       break
-    case 'PROTECT_MERGE_BUTTON':
+    case "PROTECT_MERGE_BUTTON":
       protectMergeButton()
       break
   }
