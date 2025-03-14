8c9bd1fc4030a30c0c5da45643e41fec6e3d007b
Daisuke <daisuke.kida@nijibox.co.jp>
2025-03-14 17:24:53 +0900

    feat: 要件の見直し

--- src/contents/protectMergeButton.ts
+++ src/contents/protectMergeButton.ts
@@ -1,15 +1,9 @@
-import type { PlasmoCSConfig } from "plasmo"
-
 import { observe } from "~src/utils/observe"
 import { sleep } from "~src/utils/sleep"
 import { getBooleanFromStorage } from "~src/utils/storage"
 
-export const config: PlasmoCSConfig = {
-  matches: ["https://stash.sprocket3.systems/projects/*/pull-requests/*/"]
-}
-
 export const protectMergeButton = async () => {
-  await sleep(1000)
+  await sleep(500)
   const isProtectMergeButton = await getBooleanFromStorage("isProtectMergeButton")
   const mergeButton = document.querySelector(".merge-button") as HTMLButtonElement
 
@@ -18,12 +12,14 @@ export const protectMergeButton = async () => {
     return
   }
 
+  // レビュワー1人以上から承認されている場合のみマージボタンが活性
   const reviewers = document.querySelector(".pull-request-reviewers")
   const isApproved = reviewers
-    ? !!reviewers.querySelector("span[aria-label='承認済み']")
+    ? !![...reviewers.querySelectorAll("span[aria-label='承認済み']")].length
     : false
 
   mergeButton.disabled = !isApproved
 }
 
+window.onload = protectMergeButton
 observe(".merge-button", protectMergeButton)
